9289268acd9716f0ba315fa5dd6e880935e2321d
Evan <>
2020-05-08 17:45:08 +0800

    重構

--- src/main/kotlin/Umbrella.kt
+++ src/main/kotlin/Umbrella.kt
@@ -1,9 +1,10 @@
 class Umbrella(val weather: IWeather) {
     fun totalPrice(quantity: Int, price: Int): Int {
-        if (weather.isSunny()) {
-            return quantity * (price * 0.9).toInt()
-        }
-        return quantity * price
-    }
+        var unitPrice = price
 
+        if (weather.isSunny()) {
+            unitPrice = (price * 0.9).toInt()
+        }
+        return quantity * unitPrice
+    }
 }
