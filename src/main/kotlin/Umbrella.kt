99461a3887294ed57389135325d8f0a22dfb9ba2
Evan <>
2020-05-08 17:43:46 +0800

    通過測試：晴天打9折的測試案例。購買3份，應是270元

--- src/main/kotlin/Umbrella.kt
+++ src/main/kotlin/Umbrella.kt
@@ -1,5 +1,8 @@
-class Umbrella(weather: IWeather) {
+class Umbrella(val weather: IWeather) {
     fun totalPrice(quantity: Int, price: Int): Int {
+        if (weather.isSunny()) {
+            return quantity * (price * 0.9).toInt()
+        }
         return quantity * price
     }
 
